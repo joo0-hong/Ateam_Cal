75de0e7614c733113726968abd9cbd888ee85171
hy.cra <hy.cra@review.com>
2024-07-02 16:29:28 +0900

    devide

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,6 +2,11 @@
 #include "../Project6/cal.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+  Cal* calc = new Cal();
+
+  EXPECT_EQ(2, calc->getDevide(4, 2));
+  EXPECT_EQ(0, calc->getDevide(0, 2));
+  EXPECT_EQ(0, calc->getDevide(1, 0));
+
 }
